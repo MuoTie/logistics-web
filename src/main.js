2fe9f68871401d473af1d4e6775192ba2c10ba9c
TieMuo <596139547@qq.com>
2023-03-29 21:26:59 +0800

    完成页面布局搭建

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './assets/global.css';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 Vue.config.productionTip = false
